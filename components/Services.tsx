5e36292be11436ec233a5a6a81d55e96a46ac983
kasahun-welela <kasahunwelela1@gmail.com>
2025-02-19 23:07:23 +0300

    change bg color

--- components/Services.tsx
+++ components/Services.tsx
@@ -22,7 +22,7 @@ export default function Services() {
           {serviceList.map((service: Services) => (
             <div
               key={service.id}
-              className="bg-green-500 p-6 flex flex-col md:flex-row justify-center items-center gap-3"
+              className="bg-white p-6 flex flex-col md:flex-row justify-center items-center gap-3"
             >
               <Image src="/ser.png" alt="logo" width={50} height={50} />
               <div>
@@ -35,6 +35,16 @@ export default function Services() {
           ))}
         </div>
       </div>
+      <a
+        href="#"
+        className="group relative inline-block overflow-hidden rounded border border-gray-100 bg-gray-200  px-12 py-3 text-sm font-medium text-slate-800 hover:text-violet-600 focus:outline-none focus:ring active:bg-indigo-600 active:text-white"
+      >
+        <span className="ease absolute left-0 top-0 h-0 w-0 border-t-2 border-violet-600 transition-all duration-200 group-hover:w-full"></span>
+        <span className="ease absolute right-0 top-0 h-0 w-0 border-r-2 border-violet-600 transition-all duration-200 group-hover:h-full"></span>
+        <span className="ease absolute bottom-0 right-0 h-0 w-0 border-b-2 border-violet-600 transition-all duration-200 group-hover:w-full"></span>
+        <span className="ease absolute bottom-0 left-0 h-0 w-0 border-l-2 border-violet-600 transition-all duration-200 group-hover:h-full"></span>
+        Signup
+      </a>
     </div>
   );
 }
