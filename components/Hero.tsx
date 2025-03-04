e4affcce299b85ed2fc2dd2ee0ed36aad3b30bd4
kasahun-welela <kasahunwelela1@gmail.com>
2025-03-04 17:32:58 +0300

    cleanup hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,50 +1,35 @@
 import Image from "next/image";
 import React from "react";
 import Link from "next/link";
+import students from "@/public/student.webp";
 
 export default function Hero() {
   return (
-    // <div className="bg-blue-500">
-    //   <div className="md:flex justify-between items-center  w-[80%] mx-auto ">
-    //     <div className="basis-1/2">
-    //       <h1 className="md:text-4xl font-bold text-xl text-white">
-    //         The future you imagine is closer than you think.
-    //       </h1>
-    //       <Link
-    //         href="/contact-us"
-    //         className="bg-yellow-600 text-white py-4 px-6 rounded-2xl mt-3 inline-block"
-    //       >
-    //         Contact Us
-    //       </Link>
-    //     </div>
-    //     <Image
-    //       src="/hero.png"
-    //       className="basis-1/2"
-    //       alt="hero image"
-    //       width={400}
-    //       height={500}
-    //     />
-    //   </div>
-    // </div>
-
-    <div className="mt-20 md:mt-0 h-80 md:h-screen bg-[url('/hero2.png')] md:bg-cover flex flex-col justify-center  ">
-      <div className="md:ml-40 px-3 md:max-w-[50%] ">
-        <h1 className=" text-white text-2xl md:text-5xl font-extrabold">
-          The future you imagine is closer than you think.
-        </h1>
-        <p className="py-5 text-white md:text-xl">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam,
-        </p>
-        <div className="space-x-2 ml-5">
-          <Link
-            href="/contact-us"
-            className="bg-primary text-white py-4 px-6 rounded-2xl mt-3 inline-block"
-          >
-            Contact Us
-          </Link>
+    <div className="md:flex justify-between items-center w-[90%] mx-auto py-10 mt-16">
+      <div className="md:basis-1/2 text-center md:text-left">
+        <div className="flex flex-col gap-6">
+          <h1 className="md:text-3xl lg:text-3xl text-2xl font-extrabold">
+            College apps take a community. We've got one that can{" "}
+            <span className="text-sky-400">change your life</span>
+          </h1>
+          <p className="tracking-wide text-justify text-xl">
+            Empowerly connects you with a team of college admission experts,
+            handpicked based on your interests & your goals.
+          </p>
         </div>
+        <Link
+          href="/contact-us"
+          className="bg-primary hover:opacity-80 mt-6 inline-block py-4 px-6 rounded-2xl text-white"
+        >
+          Contact Us
+        </Link>
+      </div>
+      <div className=" mt-16 md:mt-0">
+        <Image
+          src={students}
+          alt="hero image"
+          className="md:h-[450px] md:w-[550px] rounded-lg"
+        />
       </div>
     </div>
   );
