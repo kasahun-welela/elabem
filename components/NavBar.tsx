ddee6b7827f59fbed0c5a0ee6daec3ee9b8d6720
kasahun-welela <kasahunwelela1@gmail.com>
2025-03-04 13:33:41 +0300

    change mobile menu slider

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,48 +3,61 @@ import Link from "next/link";
 import React, { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
+import Image from "next/image";
 
 function NavBar() {
-  const [isOpen, setIsOpen] = useState(false);
-  const pathname = usePathname();
+  const [menuOpen, setMenuOpen] = useState(false);
+  const path = usePathname();
 
-  const toggleButton = () => {
-    setIsOpen(!isOpen);
+  const handleMenuClick = () => {
+    setMenuOpen(!menuOpen);
   };
 
-  // Animation variants for the menu
-  const menuVariants = {
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
+  // Animation variants for the mobile menu
+  const mobileMenuVariants = {
     open: {
       opacity: 1,
-      y: 0,
-      transition: { type: "spring", stiffness: 300, damping: 24 },
+      x: 0,
+      transition: {
+        type: "spring",
+        stiffness: 100,
+        damping: 20,
+        duration: 0.5,
+      },
     },
     closed: {
       opacity: 0,
-      y: "-100%",
-      transition: { duration: 0.3 },
+      x: "-100%",
+      transition: {
+        type: "spring",
+        stiffness: 100,
+        damping: 20,
+        duration: 0.5,
+      },
     },
-  };
-
-  // Animation variants for the hamburger icon
-  const iconVariants = {
-    open: { rotate: 90 },
-    closed: { rotate: 0 },
   };
 
   return (
     <div className="w-full shadow-sm">
       {/* Desktop Navigation */}
       <div className="hidden md:flex justify-around items-center py-5 md:w-[80%] mx-auto">
-        <Link href="/" className={`text-2xl font-bold text-primary`}>
-          Elabem Consultancy
-        </Link>
+        <Image
+          src="/logo.png"
+          alt="logo"
+          className="bg-primary"
+          width={200}
+          height={50}
+        />
         <ul className="flex justify-end gap-6">
           <li>
             <Link
               href="/"
               className={`${
-                pathname === "/" ? "text-primary font-bold" : ""
+                path === "/" ? "text-primary font-bold" : ""
               } hover:font-semibold hover:text-primary`}
             >
               Home
@@ -54,7 +67,7 @@ function NavBar() {
             <Link
               href="/about"
               className={`${
-                pathname === "/about" ? "text-primary font-bold" : ""
+                path === "/about" ? "text-primary font-bold" : ""
               } hover:font-semibold hover:text-primary`}
             >
               About Us
@@ -64,7 +77,7 @@ function NavBar() {
             <Link
               href="/scholarship"
               className={`${
-                pathname === "/scholarship" ? "text-primary font-bold" : ""
+                path === "/scholarship" ? "text-primary font-bold" : ""
               } hover:font-semibold hover:text-primary`}
             >
               Scholarship
@@ -74,7 +87,7 @@ function NavBar() {
             <Link
               href="/contact-us"
               className={`${
-                pathname === "/contact-us" ? "text-primary font-bold" : ""
+                path === "/contact-us" ? "text-primary font-bold" : ""
               } hover:font-semibold hover:text-primary`}
             >
               Contact Us
@@ -84,81 +97,99 @@ function NavBar() {
       </div>
 
       {/* Mobile Navigation */}
-      <div className="absolute shadow-sm top-0 right-0 left-0 md:hidden z-50">
-        <div className="flex justify-between items-center p-5">
-          <Link href="/" className="text-xl font-bold text-primary">
-            Elabem Consultancy
-          </Link>
-
-          {/* Hamburger Icon */}
-          <motion.div
-            variants={iconVariants}
-            animate={isOpen ? "open" : "closed"}
-            onClick={toggleButton}
-            className="cursor-pointer"
-          >
-            {isOpen ? (
-              <i className="ri-close-line text-primary text-3xl"></i>
-            ) : (
-              <i className="ri-menu-fill text-primary text-3xl"></i>
-            )}
-          </motion.div>
+      <div className="flex justify-between items-center max-w-[90%] mx-auto md:hidden py-5">
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            alt="logo"
+            className="bg-primary"
+            width={200}
+            height={70}
+          />
+        </Link>
+        <div className="flex items-center gap-4">
+          <button onClick={handleMenuClick} className="text-primary">
+            <i className="ri-menu-line text-2xl"></i>
+          </button>
         </div>
+      </div>
 
-        {/* Mobile Menu */}
-        <AnimatePresence>
-          {isOpen && (
-            <motion.ul
-              variants={menuVariants}
+      {/* Mobile menu nav list = */}
+      <AnimatePresence>
+        {menuOpen && (
+          <motion.div
+            className="fixed inset-0 bg-black bg-opacity-50 z-50 md:hidden"
+            onClick={closeMenu}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+          >
+            <motion.div
+              className="fixed inset-y-0 left-0 w-[75%] bg-white z-50 overflow-y-auto"
+              variants={mobileMenuVariants}
               initial="closed"
               animate="open"
               exit="closed"
-              className="flex flex-col gap-1 p-3 bg-white"
             >
-              <li>
-                <Link
-                  href="/"
-                  className={`${
-                    pathname === "/" ? "text-primary font-bold" : ""
-                  } hover:font-semibold hover:text-primary`}
-                >
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/about"
-                  className={`${
-                    pathname === "/about" ? "text-primary font-bold" : ""
-                  } hover:font-semibold hover:text-primary`}
-                >
-                  About Us
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/scholarship"
-                  className={`${
-                    pathname === "/scholarship" ? "text-primary font-bold" : ""
-                  } hover:font-semibold hover:text-primary`}
-                >
-                  Scholarship
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/contact-us"
-                  className={`${
-                    pathname === "/contact-us" ? "text-primary font-bold" : ""
-                  } hover:font-semibold hover:text-primary`}
-                >
-                  Contact Us
-                </Link>
-              </li>
-            </motion.ul>
-          )}
-        </AnimatePresence>
-      </div>
+              <div className="text-primary font-bold flex justify-between items-center p-4">
+                <h1>Elabem Consultancy</h1>
+                <button onClick={closeMenu} className="text-primary">
+                  <i className="ri-close-line text-2xl"></i>
+                </button>
+              </div>
+              <ul className="space-y-4 p-4">
+                <li>
+                  <Link
+                    className={`${
+                      path === "/" ? "text-primary font-semibold" : ""
+                    } hover:text-primary`}
+                    href="/"
+                    onClick={closeMenu}
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className={`${
+                      path === "/about" ? "text-primary font-semibold" : ""
+                    } hover:text-primary`}
+                    href="/about"
+                    onClick={closeMenu}
+                  >
+                    About Us
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className={`${
+                      path === "/scholarship"
+                        ? "text-primary font-semibold"
+                        : ""
+                    } hover:text-primary`}
+                    href="/scholarship"
+                    onClick={closeMenu}
+                  >
+                    Scholarship
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className={`${
+                      path === "/contact-us" ? "text-primary font-semibold" : ""
+                    } hover:text-primary`}
+                    href="/contact-us"
+                    onClick={closeMenu}
+                  >
+                    Contact Us
+                  </Link>
+                </li>
+              </ul>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
