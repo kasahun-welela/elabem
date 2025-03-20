0fd0b4184bb4ae877607b8d07721f4aa8130c810
kasahun-welela <kasahunwelela1@gmail.com>
2025-03-20 23:44:55 +0300

    fix: add name to function

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -8,46 +8,40 @@ import {
 } from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
 
-export default function () {
+export default function Slider() {
   const whyElabem = [
     {
       id: 1,
-      icon: "",
       title: "Our global reach",
       description:
         " We are experts in international education because we ourselves are international. Our network spans offices across the globe, as well as partnerships with universities all over the world. We also partner with over 3000 agents worldwide increasing our global reach even further.",
     },
     {
       id: 2,
-      icon: "",
       title: "Personalized Counseling",
       description:
         "We believe every student is unique. Our consultants take the time to understand your academic background, career aspirations, and personal preferences to create a customized roadmap for your  educational journey. Whether you’re aiming for top universities or specialized programs, we’ve got you covered",
     },
     {
       id: 3,
-      icon: "",
       title: " Dedicated Post-Admission Support",
       description:
         "Our relationship does not end with your acceptance letter. We offer ongoing support to help you adjust to your new academic environment, ensuring a smooth transition and long-term success. ",
     },
     {
       id: 4,
-      icon: "",
       title: "Expert Guidance",
       description:
         "At Elabem Consultancy, our team consists of seasoned education consultants with years of experience in the field. We understand the nuances of global education systems, admission processes, and scholarship opportunities, ensuring you receive the most accurate and up-to-date advice.",
     },
     {
       id: 5,
-      icon: "",
       title: "Extensive Network",
       description:
         "Elabem Consultancy has established strong relationships with leading universities and colleges worldwide. This allows us to provide exclusive insights, early application opportunities, and even fee waivers for our students.",
     },
     {
       id: 6,
-      icon: "",
       title: "End-to-End Support",
       description:
         "From course selection and application preparation to visa assistance and pre-departure briefings, we offer comprehensive support at every step. Our goal is to make the entire process seamless and stress-free for you.",
